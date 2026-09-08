2f6155cf48595bd02866527241e54d501ee7dda3
Vovasimo <kizivovan@gmail.com>
2026-09-08 23:22:27 +0300

    feat: add locations check endpoint

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Vovasimo/pip-tracker
+
+go 1.21.3
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/locations", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		json.NewEncoder(w).Encode(map[string]string{
+			"status": "ok",
+		})
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
